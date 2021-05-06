1cbf2472e3ab83803c841626800a6ba122ed145f
joaolobao380 <victor_jv380@hotmail.com>
2021-05-05 22:45:31 -0300

    Installed mysql2, aws-S3 upload provider and updated the config files

---
+++ .cache/admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoStrapi from '../../../../assets/images/maragogi_strapi.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoStrapi} alt="Prefeitura de Maragogi" />;
+
+export default Logo;
--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql2',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'h1sys_prefeiturastrapi'),
+        username: env('DATABASE_USERNAME', 'h1sys'),
+        password: env('DATABASE_PASSWORD', 'tr-server'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
